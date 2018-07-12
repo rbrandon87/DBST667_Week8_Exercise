d2c741c2208390291a3d879ad3662b2789edb021
rbrandon87 <rbrandon87@gmail.com>
2018-07-12 19:34:27 -0400

    Done
    Done

--- DBST667_Week8_Exercise_R_Script.R
+++ DBST667_Week8_Exercise_R_Script.R
@@ -8,6 +8,7 @@ library("tidyr")
 getwd()
 #Part 2b - Load dataset
 vehicle <- read.csv("vehicle.csv", header = TRUE, sep = ",")
+str(vehicle)
 #Part 2bi - Make a copy of the dataset
 myvehicle <- vehicle
 str(myvehicle)
@@ -16,5 +17,41 @@ myvehicle$Class <- NULL
 str(myvehicle)
 #Part 2biii - scale function
 myvehicle <- scale(myvehicle)
-hist(vehicle$RADIUS.RATIO)
-hist(myvehicle[,4])
+summary(vehicle)
+summary(myvehicle)
+#Part 2biv - Additional preprocessing
+#Verify no empty fields
+apply(myvehicle, 2, function (myvehicle) sum(is.na(myvehicle)))
+#Part 2ci - Run clustering
+set.seed(12345)
+kc<-kmeans(myvehicle, 4)
+#Part 2cii - kc output
+kc
+#Part 2ciii - iter output
+kc$iter
+#Part 2di - cross-tab table
+table(vehicle$Class)
+table(vehicle$Class, kc$cluster)
+#Part 2ei - Visualize the cluster
+#clusplot(kc)
+clusplot(myvehicle, kc$cluster, color=TRUE, shade=TRUE, labels=2, lines=0)
+#Part 2fi - Compare different k runs
+kc
+kc$tot.withinss
+kc$betweenss
+kc$iter
+kc<-kmeans(myvehicle, 3)
+kc
+kc$tot.withinss
+kc$betweenss
+kc$iter
+kc<-kmeans(myvehicle, 2)
+kc
+kc$tot.withinss
+kc$betweenss
+kc$iter
+kc<-kmeans(myvehicle, 1)
+kc
+kc$tot.withinss
+kc$betweenss
+kc$iter
